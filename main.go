48c6477f1422e67025c9c52982a7a6d4949d7cb8
Steven Duran <stevenduran@gmail.com>
2021-04-21 12:11:48 -0700

    Through Chapter 6

--- main.go
+++ main.go
@@ -5,52 +5,42 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/julienschmidt/httprouter"
+	"lenslocked.com/views"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email "+
-		"to <a href=\"mailto:support@lenslocked.com\">"+
-		"support@lenslocked.com</a>.")
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ</h1><h2>What is a server</h2>")
-	fmt.Fprint(w, "<p>Someone who brings your drinks</p>")
+	must(contactView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "Not all that wander are lost, but the page you are looking for is. Check the url and try again.")
+	fmt.Fprint(w, "You look lost, pardner")
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	var h http.Handler = http.HandlerFunc(notFound)
+	homeView = views.NewView("bootstrap",
+		"views/home.gohtml")
+	contactView = views.NewView("bootstrap",
+		"views/contact.gohtml")
+
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
-
-	// router := httprouter.New()
-	// router.GET("/", Index)
-	// router.GET("/hello/:name", Hello)
-
-	// log.Fatal(http.ListenAndServe(":8080", router))
 }
